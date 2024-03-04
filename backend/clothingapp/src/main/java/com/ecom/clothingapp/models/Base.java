c5ca95b93d7a80de018182bb96c327bb781b6454
Vedx23 <paturkarvedant@gmail.com>
2024-03-04 22:17:18 +0530

    build: configured application.properties

---
+++ backend/clothingapp/src/main/java/com/ecom/clothingapp/models/Base.java
@@ -0,0 +1,27 @@
+package com.ecom.clothingapp.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@MappedSuperclass
+@Setter
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Base {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+}
