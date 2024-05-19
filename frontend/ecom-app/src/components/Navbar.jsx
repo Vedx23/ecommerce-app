08885ab43800c9b4a501ef758a1b5f8e9d2365a6
Vedx23 <paturkarvedant@gmail.com>
2024-05-19 18:15:08 +0530

    feat: Home layout with filters

--- frontend/ecom-app/src/components/Navbar.jsx
+++ frontend/ecom-app/src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
                     <div className="dropdown dropdown-end">
                         <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                             <div className="w-10 rounded-full">
-                                <img alt="Tailwind CSS Navbar component" src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                <img src="" />
                             </div>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bounded-box w-52 bg-white">
